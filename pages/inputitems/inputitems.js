175fc8ec4e08254a2f349e2d0ba32d43121e0d18
whateverzpy <239275864@qq.com>
2024-01-29 21:59:05 +0800

    input界面树形选择

--- pages/inputitems/inputitems.js
+++ pages/inputitems/inputitems.js
@@ -1,13 +1,40 @@
+const chineseNumber = '一二三四五六七八九十'.split('');
+
+const generateTree = function (deep = 0, count = 10, prefix) {
+  const ans = [];
+
+  for (let i = 0; i < count; i += 1) {
+    const value = prefix ? `${prefix}-${i}` : `${i}`;
+    const rect = {
+      label: `选项${chineseNumber[i]}`,
+      value,
+    };
+
+    if (deep > 0) {
+      rect.children = generateTree(deep - 1, 10, value);
+    }
+    ans.push(rect);
+  }
+
+  return ans;
+};
+
 Page({
 	options: {
 		styleIsolation: 'apply-shared',
 	},
-
+	data: {
+		first: 0,
+		options: generateTree(1),
+		value: ['0', ['0-0', '0-1']],
+	},
 });
 
 Component({
 	data: {
-		first: 0
+		first: 0,
+		options: generateTree(1),
+		value: ['0', ['0-0', '0-1']],
 	},
 	methods: {
 		toinputitems(e) {
@@ -29,6 +56,11 @@ Component({
 			wx.redirectTo({
 				url: '/pages/advice/advice',
 			})
-		}
+		},
+		onChange(e) {
+      this.setData({
+        value: e.detail.value,
+      });
+    },
 	}
 })
