0b014f6f25c756f3e2be206aba410838c50d5069
whateverzpy <239275864@qq.com>
2024-02-05 01:29:41 +0800

    提交数据逻辑大进化！

--- pages/inputitems/inputitems.js
+++ pages/inputitems/inputitems.js
@@ -1,69 +1,69 @@
+const app = getApp()
 
 Page({
-	options: {
-		styleIsolation: 'apply-shared',
+	onShow: function() {
+		this.data.checkbox1 = this.data.checkbox1.map(checkbox => {
+			checkbox.checked = app.globalData.selected.includes(checkbox.value)
+			return checkbox;
+		})
+		this.data.checkbox2 = this.data.checkbox2.map(checkbox => {
+			checkbox.checked = app.globalData.selected.includes(checkbox.value)
+			return checkbox;
+		})
+		this.setData({
+			selected: app.globalData.selected,
+			checkbox1: this.data.checkbox1,
+			checkbox2: this.data.checkbox2
+		})
 	},
-	
-});
-
-Component({
 	data: {
 		first: 0,
-		options1: [
-      { label: '全选', checkAll: true },
-      { label: '选项1', value: "选项1" },
-      { label: '选项2', value: "选项2" },
-      {
-        label: '选项3',
-        value: "选项3",
-        content: '描述信息',
-      },
+		checkbox1: [
+      { value: '选项一', checked: false },
+      { value: '选项二', checked: false },
+      { value: '选项三', checked: false }
 		],
-		options2: [
-      { label: '全选', checkAll: true },
-      { label: '选项4', value: "选项4" },
-      { label: '选项5', value: "选项5" },
-      {
-        label: '选项6',
-        value: "选项6",
-        content: '描述信息',
-      },
-    ],
+		checkbox2: [
+      { value: '选项四', checked: false },
+      { value: '选项五', checked: false },
+      { value: '选项六', checked: false }
+		],
 	},
-	methods: {
-		toinputitems(e) {
-			wx.redirectTo({
-				url: '/pages/inputitems/inputitems',
-			})
-		},
-		tomychoice(e) {
-			wx.redirectTo({
-				url: '/pages/mychoice/mychoice',
-			})
-		},
-		topartnerchoice(e) {
-			wx.redirectTo({
-				url: '/pages/partnerchoice/partnerchoice',
-			})
-		},
-		toadvice(e) {
-			wx.redirectTo({
-				url: '/pages/advice/advice',
-			})
-		},
-		onCheckAllChange(event) {
-			console.log('checkbox', event.detail);
-			event.detail.context.checked = !event.detail.context.checked
+	toinputitems: function() {
+		wx.redirectTo({
+			url: '/pages/inputitems/inputitems',
+		})
+	},
+	tomychoice: function() {
+		wx.redirectTo({
+			url: '/pages/mychoice/mychoice',
+		})
+		console.log(app.globalData.selected)
+	},
+	topartnerchoice: function() {
+		wx.redirectTo({
+			url: '/pages/partnerchoice/partnerchoice',
+		})
+	},
+	toadvice: function() {
+		wx.redirectTo({
+			url: '/pages/advice/advice',
+		})
+	},
+	onCheckAllChange: function(e) {
+		console.log('checkbox', e.detail.value)
       this.setData({
-        checkAllValues: event.detail,
-      });
-		},
-		formReset(e) {
-			console.log('form发生了reset事件，携带数据为：', e.detail.value)
-			this.setData({
-				chosen: ''
+        checkAllValues: e.detail.value
 			})
-		},
+			app.globalData.selected = e.detail.value
 	},
-	behaviors: ['wx://form-field-group']
+	resetCheckbox: function() {
+		app.globalData.selected = []
+		this.setData({
+			selected: app.globalData.selected
+		})
+		wx.redirectTo({
+			url: '/pages/inputitems/inputitems',
+		})
+	}
 })
